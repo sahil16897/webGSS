96a2b521de03f30a1d014e2c71e0c31b8631f29e
Sahil Kumar <sahilkumar16897@gmail.com>
2021-06-02 12:09:28 +0530

    website hosting

---
+++ login_student.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Gayatri Shiksha Sadan : Student Login</title>
+  <link rel="shortcut icon" type="image/icon" href="img/gallery/7.jpg"/>
+
+
+  <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+    <form class="form-signin" action="process.php" method="post">
+      <h2 class="form-signin-heading"><center>Student's Account</center></h2>
+      <input type="text" class="form-control" id="user" name="username" placeholder="Registration Number" required="" autofocus="" />
+      <input type="password" class="form-control" id="pass" name="password" placeholder="Password" required=""/>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+      <p id="p01"><a href="index.html"><center>Back to Home Page</center></a></p>
+    </form>
+
+  </div>
+
+
+
+</body>
+
+</html>
